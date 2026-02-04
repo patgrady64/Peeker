a49fe18997c25f6545000423435943d7d40f240c
patgrady64 <patgrady64@gmail.com>
2026-02-04 13:18:05 -0500

    Fixed imports to make code more readable

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import Deck
-import Player
-import HandAnalyzer
+from Deck import Deck
+from Player import Player
+from HandAnalyzer import HandAnalyzer
 
 if __name__ == '__main__':
-    deck = Deck.Deck()
-    player = Player.Player()
+    deck = Deck()
+    player = Player()
     # for i in range(5):
     #     player.add(deck.dealOne())
     player.inject(["4s", "5c", "6s", "7s", "8s"], deck)
     player.print_cards()
-    analyzed = HandAnalyzer.HandAnalyzer(player.get_cards, deck.get_cards)
+    analyzed = HandAnalyzer(player.get_cards, deck.get_cards)
     analyzed.analyze()
     analyzed.print_hand_rank()
