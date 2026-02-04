fec1f5f3b30319c1cba45526a781d480f47e32a4
patgrady64 <patgrady64@gmail.com>
2026-02-04 12:34:11 -0500

    Refactored Card to use lookup tables

--- Card.py
+++ Card.py
@@ -2,75 +2,22 @@ class Card(object):
     def __init__(self, suit, value):
         self.value = value
         self.suit = suit
-        self.full_value = None
-        self.int_value = None
-        self.int_suit = None
-        self.full_suit = None
-
-        match self.value:
-            case "2":
-                self.full_value = "Two"
-                self.int_value = 2
-            case "3":
-                self.full_value = "Three"
-                self.int_value = 3
-            case "4":
-                self.full_value = "Four"
-                self.int_value = 4
-            case "5":
-                self.full_value = "Five"
-                self.int_value = 5
-            case "6":
-                self.full_value = "Six"
-                self.int_value = 6
-            case "7":
-                self.full_value = "Seven"
-                self.int_value = 7
-            case "8":
-                self.full_value = "Eight"
-                self.int_value = 8
-            case "9":
-                self.full_value = "Nine"
-                self.int_value = 9
-            case "t":
-                self.full_value = "Ten"
-                self.int_value = 10
-            case "j":
-                self.full_value = "Jack"
-                self.int_value = 11
-            case "q":
-                self.full_value = "Queen"
-                self.int_value = 12
-            case "k":
-                self.full_value = "King"
-                self.int_value = 13
-            case "a":
-                self.full_value = "Ace"
-                self.int_value = 14
-        match suit:
-            case "c":
-                self.full_suit = "Clubs"
-                self.int_suit = 1
-            case "d":
-                self.full_suit = "Diamonds"
-                self.int_suit = 2
-            case "h":
-                self.full_suit = "Hearts"
-                self.int_suit = 3
-            case "s":
-                self.full_suit = "Spades"
-                self.int_suit = 4
+        self.full_value_lookup = {"2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine", "t": "Ten", "j": "Jack", "q": "Queen", "k": "King", "a": "Ace"}
+        self.full_value_plural_lookup = {"2": "Twos", "3": "Threes", "4": "Fours", "5": "Fives", "6": "Sixes", "7": "Sevens", "8": "Eights", "9": "Nines", "t": "Tens", "j": "Jacks", "q": "Queens", "k": "Kings", "a": "Aces"}
+        self.int_value_lookup = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "t": 10, "j": 11, "q": 12, "k": 13, "a": 14}
+        self.int_suit = {"c": 1, "s": 2, "h": 3, "d": 4}
+        self.full_suit_lookup = {"c": "Clubs", "s": "Spades", "h": "Hearts", "d": "Diamonds"}
 
     def print_full(self):
-        print("{} of {}".format(self.full_value, self.full_suit))
+        print(f"{self.full_value_lookup[self.value]} of {self.full_suit_lookup[self.suit]}")
 
     def print_mini(self):
-        print("{}{}".format(self.value,self.suit))
+        print(f"{self.value}{self.suit}")
 
     @property
     def get_int_value(self):
-        return self.int_value
+        return self.int_value_lookup[self.value]
 
     @property
     def get_int_suit(self):
-        return self.int_suit
+        return self.full_suit_lookup[self.suit]
